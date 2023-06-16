ed76ef801db5ef5445746c461c0a023878b80fb9
Mihail Stoykov <M.Stoikov@gmail.com>
2023-06-16 11:34:48 +0300

    Add otter and some typescript stuff

--- init.lua
+++ init.lua
@@ -523,6 +523,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'otter' },
   },
 }
 
--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -9,7 +9,6 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.diagnostics.vale,
         null_ls.builtins.diagnostics.golangci_lint,
