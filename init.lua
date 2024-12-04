d2360b27e34ba0f40e861fa54d18b8c9e9ac1f96
Mihail Stoykov <M.Stoikov@gmail.com>
2024-12-04 11:45:12 +0200

    tsserver is now ts_ls

--- init.lua
+++ init.lua
@@ -486,7 +486,7 @@ local servers = {
   },
   -- pyright = {},
   -- rust_analyzer = {},
-  tsserver = {},
+  ts_ls = {},
   golangci_lint_ls = {},
 
   lua_ls = {
@@ -509,16 +509,15 @@ local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = servers[server_name],
+      }
+    end,
+  }
 }
 
 -- [[ Configure nvim-cmp ]]
